357d6fd8c1c2b15ec7e5103feedf269c48c700af
henrry-sakurazaka <bright.lights.21@me.com>
2026-02-07 13:27:30 +0900

    Update logout function

--- src/Dashboad.tsx
+++ src/Dashboad.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "leaflet/dist/leaflet.css";
 import { MapProvider } from './context/StateContext';
 import { useAuth } from './context/AuthContext';
+import { useNavigate } from "react-router-dom";
 import SearchForm from './components/SearchForm';
 import StoreList from './components/StoreList';
 import MapCanvas from './components/MapCanvas';
@@ -14,13 +15,18 @@ const Dashboad: React.FC = () => {
   const handleSearch = (lat: number, lon: number) => {
     setCenter([lat, lon]);
   };
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/")
+  }
 
   return (   
      <div className="relative flex flex-col bg-gray-700 h-screen">
       
       {/* ② 右上ログアウトボタン */}
       <button
-        onClick={logout}
+        onClick={handleLogout}
         className="absolute top-4 right-4 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
       >
         ログアウト
