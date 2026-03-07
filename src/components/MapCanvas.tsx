1fad24ffe13ff96a149e1917caa51ab9ed2cb6d1
henrry-sakurazaka <bright.lights.21@me.com>
2026-03-07 12:25:30 +0900

    マップのマーカーが表示されるようになりました

--- src/components/MapCanvas.tsx
+++ src/components/MapCanvas.tsx
@@ -3,6 +3,18 @@ import L from "leaflet";
 import type { LatLngExpression } from "leaflet";
 import { useMapStore } from "../context/StateContext";
 import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 interface MapCanvasProps {
   center?: LatLngExpression;
